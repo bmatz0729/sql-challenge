3d2f405e036ff4b2914c5794b3a9eeb77301adbb
Bmatz0729 <bmartinez0729@gmail.com>
2019-10-24 05:50:17 -0400

    Updating sql files

---
+++ Employee_Department.sql
@@ -0,0 +1,79 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE Title (
+    emp_no int   NOT NULL,
+    title varchar(30)   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+CREATE TABLE Salary (
+    emp_no int   NOT NULL,
+    title varchar(200)   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+CREATE TABLE Employee (
+    emp_no int   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name varchar(30)   NOT NULL,
+    last_name varchar(30)   NOT NULL,
+    gender varchar(30)   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_Employee PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Dept_manager (
+    dept_no varchar(20)   NOT NULL,
+    emp_no int   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+CREATE TABLE Dept_emp (
+    emp_no int   NOT NULL,
+    dept_no varchar(20)   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+CREATE TABLE Departments (
+    dept_no varchar(20)   NOT NULL,
+    dept_name varchar(200)   NOT NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+ALTER TABLE Title ADD CONSTRAINT fk_Title_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Salary ADD CONSTRAINT fk_Salary_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Dept_manager ADD CONSTRAINT fk_Dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Dept_manager ADD CONSTRAINT fk_Dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Dept_emp ADD CONSTRAINT fk_Dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Dept_emp ADD CONSTRAINT fk_Dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+CREATE INDEX idx_Employee_first_name
+ON Employee (first_name);
+
+CREATE INDEX idx_Employee_last_name
+ON Employee (last_name);
+
